831c0093195071ec27905c7fc08be0a4b34c6cfd
Meirlen <miko_982@mail.ru>
2022-11-14 16:54:32 +0600

    change yandex configs

--- run_order_status_listener.py
+++ run_order_status_listener.py
@@ -1,29 +1,27 @@
 
 
-import threading
+from yandex.abc import *
 
-
-def get_count():
-    counter = 0
-
-    threading.Timer(5.0, get_count).start()
-    counter+=1
-    records = get_records()
-    print("Getting count...", str(records))
-
-
-    return str(counter)
-
-import random
-def get_records():
-
-    return random.randint(3, 9)
-
-    
-get_count()
+run_get_status_def()
 
 
 
 
+# import threading
 
 
+# def get_count():
+#     counter = 0
+
+#     threading.Timer(5.0, get_count).start()
+#     counter+=1
+#     records = get_records()
+#     print("Getting count...", str(records))
+
+
+#     return str(counter)
+
+# import random
+# def get_records():
+
+#     return random.randint(3, 9)
