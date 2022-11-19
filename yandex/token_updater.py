a5ff9537334a62c411be2f6660866b154f96f648
Meirlen <miko_982@mail.ru>
2022-11-19 23:13:38 +0600

    add order status listener

--- yandex/token_updater.py
+++ yandex/token_updater.py
@@ -2,16 +2,8 @@ import requests
 from yandex.json_data import *
 from yandex.yandex_config import * 
 import json
-
-
-def update_yandex_token(new_token):
-    
-
-    json_data = {
-                "token": new_token
-                }
-    requests.post('http://165.22.13.172:8000/webhook/yandex_token_update',json=json_data)
-
+import aiohttp
+import asyncio
 
 
 def get_new_token():
@@ -36,13 +28,35 @@ def get_new_token():
     return None
 
 
+async def get_order(session, new_token):
+
+    url = 'http://165.22.13.172:8000/webhook/yandex_token_update'
+
+    json_data = {
+                "token": new_token
+                }
+    async with session.post(url, json = json_data) as response:
+        order = await response.text()
+        print("-> Token updated" )
+
+        return order
+
+async def get_orders_status(token):
+
+    async with aiohttp.ClientSession() as session:
+
+        tasks = []
+
+        tasks.append(asyncio.ensure_future(get_order(session,token)))
+
+        await asyncio.gather(*tasks)
 
 import time
 def update_token_by_timer():
     while True :
         print("Yandex token successfully updated")
         new_token = get_new_token()
-        update_yandex_token(new_token)
+        asyncio.run(get_orders_status(new_token))
         time.sleep(1800)
 
 # update_token_by_timer()
